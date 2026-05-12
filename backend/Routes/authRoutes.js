508b3596585332bb0b89847625e2ea1c21576d37
Marfidha <marfidhaap@gmail.com>
2026-05-12 22:02:33 +0530

    update server

--- backend/Routes/authRoutes.js
+++ backend/Routes/authRoutes.js
@@ -1,11 +1,11 @@
 import express from "express"
 import { registerUser, syncUser } from "../controllers/authcontroller.js";
-import {  clerkMiddleware } from "@clerk/express";
+import {   requireAuth } from "@clerk/express";
 
 
 const router = express.Router();
 
 router.post("/register", registerUser);
-router.post("/sync", clerkMiddleware(), syncUser);
+router.post("/sync", requireAuth, syncUser);
 
 export default router;
