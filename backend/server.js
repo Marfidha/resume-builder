fa3d3ef5be996f155c9d2136d3d57fd4b0a73956
Marfidha <marfidhaap@gmail.com>
2026-05-12 12:16:30 +0530

    prepared backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,21 @@ app.use(express.json());
 connectDB()
 const port = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
-    credentials: true,
-  })
-);
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  process.env.CLIENT_URL_PROD
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 app.use("/api/users", authRoutes);
 app.use('/api/resumes', resumeRoutes);
